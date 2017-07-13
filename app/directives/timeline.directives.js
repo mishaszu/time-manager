1a21963bb5a67ae72c4664852fbc3028b8da43c7
Michal Szulczewski <michal.szulczewski@scalac.io>
2017-07-13 13:36:57 +0200

    create simple timeline

--- app/directives/timeline.directives.js
+++ app/directives/timeline.directives.js
@@ -4,6 +4,26 @@ app.directive('timeline', function(){
     scope: {
       data: "="
     },
-    templateUrl: "directives/timeline.directives.html"
+    templateUrl: "directives/timeline.directives.html",
+    link: function(scope, element, attr) {
+      var ww = window.innerWidth,
+          maxW = 2400,
+          time = new Date(),
+          hour = time.getHours(),
+          minutes = time.getMinutes(),
+          currentMargin = 0,
+          element = document.getElementsByClassName('tl')[0];
+
+      element.style.marginLeft = -1 * ( (hour - 1) * 100 + hour * 1 ) + (ww / 2) - ((minutes*100)/60) + "px";
+
+      var interval = setInterval(addMargin, 60000);
+      
+      function addMargin() {
+        var time = new Date(),
+        hour = time.getHours(),
+        minutes = time.getMinutes();
+        element.style.marginLeft = -1 * ( (hour - 1) * 100 + hour * 1 ) + (ww / 2) - ((minutes*100)/60) + "px";
+      }
+    }
   }
 });
