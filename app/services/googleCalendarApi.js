cf4750f3a31cadc42c460a519f016af0785e5302
Michal Szulczewski <michal.szulczewski@scalac.io>
2017-07-20 16:16:29 +0200

    clean google service and css

--- app/services/googleCalendarApi.js
+++ app/services/googleCalendarApi.js
@@ -1,46 +1,58 @@
-app.factory('GoogleCalendarApi', ['$http', function($http){
-  var CLIENT_ID = "",
+app.factory('GoogleCalendarApi', ['$http', function ($http) {
+  var button = document.getElementById ('login'),
+      button2 = document.getElementById ('logout'),
+      CLIENT_ID = "",
       DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-      SCOPES = "https://www.googleapis.com/auth/calendar.readonly",
-      gapiStatus = false,
       eventsArray = [],
-      button = document.getElementById('login'),
-      button2 = document.getElementById('logout');
+      SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
   function setUpClient (client) {
     CLIENT_ID = client;
   }
 
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
+  function handleClientLoad () {
+    gapi.load ('client:auth2', initClient);
   }
 
-  function initClient() {
-    gapi.client.init({
+  function initClient () {
+    gapi.client.init ({
       discoveryDocs: DISCOVERY_DOCS,
       clientId: CLIENT_ID,
       scope: SCOPES
-    }).then(function () {
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    }).then (function () {
+      gapi.auth2.getAuthInstance ().isSignedIn.listen (updateSigninStatus);
+      updateSigninStatus (gapi.auth2.getAuthInstance ().isSignedIn.get());
       button.onclick = signIn;
       button2.onclick = logout;
     });
   }
 
-  function updateSigninStatus(status) {
-    var arr = [];
+  function updateSigninStatus (status) {
+    var arr = [],
+        res = [],
+        count = 0,
+        color = "",
+        preparedDiv = "",
+        sortedElements = [];
+    
     if (status) {
-      console.log("zalogowany");
-      var res = getCalendarList();
-      res.then(function(data){
+      console.log ("zalogowany");
+      res = getCalendarList();
+      res.then(function (data) {
         eventsArray = filterCalendarsIds(data);
-        Promise.all(eventsArray).then(function(data){
-          data.forEach(function(cal){
-            if(cal.result.items.length) {
-              cal.result.items.forEach(function(event){
+        Promise.all(eventsArray[0]).then(function (data) {
+          data.forEach(function (cal) {
+            color = eventsArray[1][count];
+            count++;
+            if (cal.result.items.length) {
+              cal.result.items.forEach(function (event) {
                 if (event.summary, event.start.dateTime, event.end.dateTime) {
-                  var preparedDiv = prepareAndDisplay(event.summary, event.start.dateTime, event.end.dateTime);
+                    preparedDiv = prepareAndDisplay(
+                      event.summary, 
+                      event.start.dateTime, 
+                      event.end.dateTime,
+                      color
+                    );
                   if (preparedDiv) {
                     arr.push(preparedDiv);
                   }
@@ -48,7 +60,7 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
               });
             }
           });
-          var sortedElements = sortElementArray(arr);
+          sortedElements = sortElementArray(arr);
           marginateFromTop(sortedElements);
         })
       });
@@ -62,7 +74,7 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
    	if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
       gapi.auth2.getAuthInstance().signIn()
     } else {
-      console.log("logged");
+      console.log("arleady logged");
     }
   }
   
@@ -71,23 +83,26 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
   }
 
   function getCalendarList () {
-    var request = gapi.client.calendar.calendarList.list();
-    var response = new Promise(function(resolve) {
-      request.execute(function(resp) {
-        resolve(resp.items);
-      });
-    });
+    var request = gapi.client.calendar.calendarList.list(),
+        response = new Promise(function (resolve) {
+          request.execute(function (resp) {
+            resolve(resp.items);
+          });
+        });
     return response;;
   }
 
   function filterCalendarsIds (array) {
     var calendarsArr = array,
         eventsArr = [],
+        colorArr = [],
         date = (new Date()).toISOString();
-    calendarsArr.forEach(function(calendar) {
+    
+    calendarsArr.forEach(function (calendar) {
       eventsArr.push(eventsRequest(calendar.id, date));
+      colorArr.push(calendar.backgroundColor);
     });
-    return eventsArr;
+    return [ eventsArr , colorArr ];
   }
 
   function eventsRequest (id, date) {
@@ -101,61 +116,75 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
     });
   }
 
-  function prepareAndDisplay (id, start, end) {
-    var root = document.getElementById("displayer");
-    var tStart = new Date(start),
+  function prepareAndDisplay (id, start, end, color) {
+    var root = document.getElementById("displayer"),
+        tStart = new Date(start),
         tEnd = new Date(end),
-        startMargin = 0;
-        diff = (tEnd.getTime() - tStart.getTime())/60000;
-
-    var hour = tStart.getHours(),
-        minutes = tStart.getMinutes();
+        startMargin = 0,
+        diff = (tEnd.getTime() - tStart.getTime())/60000,
+        hour = tStart.getHours(),
+        minutes = tStart.getMinutes(),
+        element = "",
+        text = "";
+        
     if (new Date().getDay() === tStart.getDay()) {
-      var element = document.createElement("div");
-
+      element = document.createElement("div");
       startMargin = (hour - 1) * 100;
       if (minutes > 0) {
         startMargin += minutes*100/60;
       }
-
+      
       element.style.width = diff*100/60 + "px";
       element.style.height = "30px";
       element.style.position = "absolute";
       element.style.marginLeft = startMargin + "px";
+      element.style.backgroundColor = color;
       element.className = "event";
-      var text = document.createTextNode(id);
+      text = document.createTextNode(id);
+      
       element.TM = {};
       element.TM.start = startMargin;
       element.TM.end = startMargin + diff*100/60;
       element.TM.wRatio = 0;
+      
       element.appendChild(text);
       root.appendChild(element);
+      
       return element;
     }
-
-    function cWidth(minutes) {
+    
+    function cWidth (minutes) {
       return minutes * 100 / 60;
     }
   }
 
   function sortElementArray (arr) {
     var map = [],
-        id = 0;
-    arr.sort(function(a, b) {
+        id = 0,
+        item = "",
+        item2 = "",
+        startTime = 0;
+        arr2 = [],
+        x = 0;
+
+    arr.sort(function (a, b) {
       return a.TM.start - b.TM.start;
     });
-    arr.forEach(function(ev){
+
+    arr.forEach(function (ev) {
       ev.TM.id = id;
       ev.TM.r = -1;
       id++;
     })
     
-    var item = arr[arr.length - 1];
-    var startTime = item.start;
-    var arr2 = [item];
-    var x = arr.length - 2;
+    item = arr[arr.length - 1];
+    startTime = item.start;
+    arr2 = [item];
+    x = arr.length - 2;
+    
     for (; x >= 0; x--) {
-      var item2 = arr[x];
+      item2 = arr[x];
+      
       if (item2.TM.end > item.TM.start) {
         arr2.push(item2);
       } else {
@@ -164,6 +193,7 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
         arr2 = [item];
         x = item.TM.id;
       }
+      
       if (x === 0) {
         map.push(arr2);
       }
@@ -171,18 +201,27 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
     return map;
   } 
   
-  function marginateFromTop(arr) {
-    arr.forEach(function(subArr){
-      var length = subArr.length; 
-      var marginArr = [];
+  function marginateFromTop (arr) {
+    var length = 0,
+        marginArr = [],
+        number = 0,
+        index = 0,
+        marign = 0;
+    
+    arr.forEach(function (subArr) {
+      length = subArr.length; 
+      marginArr = [];
+      
       for (var x = 0, max = length; x < max; x++) {
-        var number = x;
+        number = x;
         marginArr.push(number*30);
       }
-      subArr.forEach(function(ev){
-        var index = marginArr.indexOf(ev.TM.r);
+      
+      subArr.forEach(function (ev) {
+        index = marginArr.indexOf(ev.TM.r);
+        
         if (index > -1) {
-          var margin = marginArr.splice(index,1);
+          margin = marginArr.splice(index,1);
           ev.style.marginTop = ev.TM.r + "px";
         } else {
           ev.TM.r = marginArr[0];
@@ -193,10 +232,8 @@ app.factory('GoogleCalendarApi', ['$http', function($http){
     })
   }
 
-
   return {
     setUpClient: setUpClient,
     handleClientLoad: handleClientLoad,
-    eventsArray: eventsArray
   }
 }]);
